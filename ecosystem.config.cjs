ac5cb3f2b4980997ae1485fd8cbb409ba2c918ad
Marc Cracco <709406+marc2912@users.noreply.github.com>
2025-02-04 19:50:06 -0500

    Adding pm2 config file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "splitflap",
+      script: "/opt/splitflap/src/server.ts",
+      interpreter: "/home/splitflap/.npm-global/bin/ts-node",
+      node_args: "--loader ts-node/esm",
+      watch: false,
+      autorestart: true,
+      env: {
+        NODE_ENV: "production",
+        NODE_OPTIONS: "--loader ts-node/esm"
+      }
+    }
+  ]
+};
