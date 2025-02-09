007fe6f90932758aeb8d2ed0edfbc2d6750ee6e6
Marc Cracco <709406+marc2912@users.noreply.github.com>
2025-02-08 21:06:29 -0500

    create admin reset functionality

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,7 @@
 import express , { Request, Response } from "express";
 import { getEsp32MacPrefixes } from "../utils/hardware_manager";
+import { loadConfig, saveConfig } from "../utils/config_handler";
+
 const { logger } = require("../utils/logger");
 const router = express.Router();
 
@@ -7,6 +9,13 @@ router.post("/get-mac-prefixes", async (req: Request, res: Response) => {
 
     const esp32Prefixes = await getEsp32MacPrefixes();
     res.json({ esp32Prefixes });
+});
+
+router.post("/reset-env", async (req: Request, res: Response) => {
+
+    logger.info("Resetting environment");
+    saveConfig({ modules: [], layout: [], pairingKey: "" });
+    res.json({ message: "Environment reset" });
 });
 
 export default router;
